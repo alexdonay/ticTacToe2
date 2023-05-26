86bf18b1e6468db496efb86895e1e1ad9c17b972
Alexsander <alexdonay@gmail.com>
2023-05-26 20:53:56 -0300

    Adicionar arquivos de projeto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace ticTacToe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pbPedra = new System.Windows.Forms.PictureBox();
+            this.pbPapel = new System.Windows.Forms.PictureBox();
+            this.pbTesoura = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPedra)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPapel)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbTesoura)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbPedra
+            // 
+            this.pbPedra.Location = new System.Drawing.Point(64, 62);
+            this.pbPedra.Name = "pbPedra";
+            this.pbPedra.Size = new System.Drawing.Size(100, 100);
+            this.pbPedra.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbPedra.TabIndex = 0;
+            this.pbPedra.TabStop = false;
+            this.pbPedra.Click += new System.EventHandler(this.pbPedra_Click);
+            // 
+            // pbPapel
+            // 
+            this.pbPapel.Location = new System.Drawing.Point(200, 62);
+            this.pbPapel.Name = "pbPapel";
+            this.pbPapel.Size = new System.Drawing.Size(100, 100);
+            this.pbPapel.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbPapel.TabIndex = 1;
+            this.pbPapel.TabStop = false;
+            this.pbPapel.Click += new System.EventHandler(this.pbPapel_Click);
+            // 
+            // pbTesoura
+            // 
+            this.pbTesoura.Location = new System.Drawing.Point(338, 62);
+            this.pbTesoura.Name = "pbTesoura";
+            this.pbTesoura.Size = new System.Drawing.Size(100, 100);
+            this.pbTesoura.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbTesoura.TabIndex = 2;
+            this.pbTesoura.TabStop = false;
+            this.pbTesoura.Click += new System.EventHandler(this.pbTesoura_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pbTesoura);
+            this.Controls.Add(this.pbPapel);
+            this.Controls.Add(this.pbPedra);
+            this.Name = "Form1";
+            this.Text = "TicTacToe";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbPedra)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPapel)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbTesoura)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbPedra;
+        private System.Windows.Forms.PictureBox pbPapel;
+        private System.Windows.Forms.PictureBox pbTesoura;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ticTacToe
+{
+    public partial class Form1 : Form
+    {
+        private enum Escolha { pedra, papel, tesoura }
+        private Escolha escolhaUsuario;
+        private Escolha escolhaComputador;
+        private readonly Image pedraImg = Image.FromFile((Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "img", "pedra.png")));
+        private readonly Image papelImg = Image.FromFile((Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "img", "papel.png")));
+        private readonly Image tesouraImg = Image.FromFile((Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "img", "tesoura.png")));
+        public Form1()
+        {
+            
+            InitializeComponent();
+          }
+         
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pbPapel.Image = papelImg;
+            pbPedra.Image = pedraImg;
+            pbTesoura.Image = tesouraImg;
+
+        }
+
+        private void pbPedra_Click(object sender, EventArgs e)
+        {
+            Escolha escolhaComputador = gerarApostaComputador();
+            Escolha escolhaHumano = Escolha.pedra;
+            bool humanoGanhou = VerificaHumanoGanhou(escolhaHumano,escolhaComputador);
+            MessageBox.Show(humanoGanhou ? "Você Ganhou" : "Você Perdeu");
+
+        }
+
+
+    
+        private bool VerificaHumanoGanhou(Escolha escolhaHumano, Escolha escolhaComputador)
+        {
+            switch (escolhaComputador)
+            {
+                case Escolha.papel:
+                    switch (escolhaHumano)
+                    {
+                        case Escolha.papel:
+                            return false;
+                        case Escolha.pedra:
+                            return false;
+                        case Escolha.tesoura:
+                            return true;
+                    }
+                    break; 
+                case Escolha.pedra:
+                    switch (escolhaHumano)
+                    {
+                        case Escolha.papel:
+                            return true;
+                        case Escolha.pedra:
+                            return false;
+                        case Escolha.tesoura:
+                            return false;
+                    }
+                    break; 
+                case Escolha.tesoura:
+                    switch (escolhaHumano)
+                    {
+                        case Escolha.papel:
+                            return false;
+                        case Escolha.pedra:
+                            return true;
+                        case Escolha.tesoura:
+                            return false;
+                    }
+                    break;
+            }
+
+            return false; 
+        }
+        private Escolha gerarApostaComputador()
+        {
+            Random random = new Random();
+            int numeroAleatorio = random.Next(3);
+            switch (numeroAleatorio)
+            {
+                case 1:
+                    {
+                        MessageBox.Show("O computador Escolhe Pedra");
+                        return Escolha.pedra;
+                    }
+                     
+                case 2:
+                    {
+                        MessageBox.Show("O computador Escolheu Papel");
+                        return Escolha.papel;
+                    }
+                case 0:
+                    {
+                        MessageBox.Show("O computador Escolheu Tesoura");
+                        return Escolha.tesoura;
+                    }
+                default: return Escolha.papel;
+            }
+        }
+
+        private void pbPapel_Click(object sender, EventArgs e)
+        {
+            Escolha escolhaComputador = gerarApostaComputador();
+            Escolha escolhaHumano = Escolha.papel;
+            bool humanoGanhou = VerificaHumanoGanhou(escolhaHumano, escolhaComputador);
+            MessageBox.Show(humanoGanhou ? "Você Ganhou" : "Você Perdeu");
+        }
+
+        private void pbTesoura_Click(object sender, EventArgs e)
+        {
+            Escolha escolhaComputador = gerarApostaComputador();
+            Escolha escolhaHumano = Escolha.tesoura;
+            bool humanoGanhou = VerificaHumanoGanhou(escolhaHumano, escolhaComputador);
+            MessageBox.Show(humanoGanhou ? "Você Ganhou" : "Você Perdeu");
+        }
+    }
+}
